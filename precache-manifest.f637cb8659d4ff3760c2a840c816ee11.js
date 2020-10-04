79a1eebb6d028bd77fefd07846b6437458997258
MAKAR190 <lutskyimakar@gmail.com>
2020-10-04 12:54:52 +0300

    Updates

---
+++ precache-manifest.f637cb8659d4ff3760c2a840c816ee11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5634a585d141cdd2e209dc6fb3d0c37",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "597303955bd0d4675178",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.f39e210b.chunk.css"
+  },
+  {
+    "revision": "f4d91274b90f9a862bb6",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.60228937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.60228937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597303955bd0d4675178",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.50bd46a1.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
